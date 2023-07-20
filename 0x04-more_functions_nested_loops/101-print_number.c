aec4c228cd9d30b845759809125fd04dd72d0e14
zanelemabuza20 <zanelemabuza20@gmail.com>
2023-07-20 15:11:07 -0700

    prime number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,26 +1,35 @@
 #include "main.h"
 
 /**
- * Print_number - prints an integer
- * @n: integer to be printed
+ * print_number - prints an integer
+ * @n: input integer
+ * Return: no return
  */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int m, d, count;
 
-	if(n < 0)
+	if (n < 0)
 	{
-		n1 = -n;
-		_putchar('_');
-	} else 
+		_putchar(45);
+		m = n * -1;
+	}
+	else
 	{
-		n1= n;
+		m = n;
 	}
 
-	if(n1/ 10)
+	d = m;
+	count = 1;
+
+	while (d > 9)
 	{
-		print_number(n1/ 10);
+		d /= 10;
+		count *= 10;
 	}
 
-	_putchar((n1 % 10 + '0'));
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }

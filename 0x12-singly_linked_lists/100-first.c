3972e15ff2ef300e9f260332dc01183767e7c225
zanelemabuza20 <zanelemabuza20@gmail.com>
2023-08-24 08:53:56 -0700

    Task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * pre_main - func runs before main
+ * Return: void (Success)
+ */
+void __attribute__ ((constructor)) pre_main()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
